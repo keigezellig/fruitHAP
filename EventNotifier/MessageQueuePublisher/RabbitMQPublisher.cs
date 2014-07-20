cd401b3f6cf29e5d4ff189e7d6c571bfd25c9b9e
keigezellig <keigezellig@dunav.yugo>
2014-07-20 17:47:08 +0200

    - Added rabbit mq publisher - Rearrange project structure

---
+++ EventNotifier/MessageQueuePublisher/RabbitMQPublisher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using EasyNetQ;
+using EventNotifierService.Common;
+using EventNotifierService.Common.Messages;
+
+namespace DoorPi.MessageQueuePublisher
+{
+    public class RabbitMQPublisher : IMQPublisher
+    {
+        private IBus messageBus;
+
+        public RabbitMQPublisher(string connectionString)
+        {
+            messageBus = CreateMessageBus(connectionString);
+        }
+        
+        
+        public void Publish(DoorMessage message)
+        {
+            messageBus.Publish(message);
+        }
+
+
+        private IBus CreateMessageBus(string connectionString)
+        {
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new Exception("MQ connection string is missing");
+            }
+            return RabbitHutch.CreateBus(connectionString);
+        }
+
+        public void Dispose()
+        {
+            messageBus.Dispose();
+        }
+    }
+}
