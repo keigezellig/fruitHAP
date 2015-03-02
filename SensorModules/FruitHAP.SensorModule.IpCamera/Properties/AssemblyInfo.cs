fba0e36b3de83e2d01e82866abb66c17124dc321
keigezellig <keigezellig@dunav.yugo>
2015-03-02 19:01:11 +0100

    Issue #14: Fixed version numbering scheme to major.minor.fixversion.build

--- SensorModules/FruitHAP.SensorModule.IpCamera/Properties/AssemblyInfo.cs
+++ SensorModules/FruitHAP.SensorModule.IpCamera/Properties/AssemblyInfo.cs
@@ -29,8 +29,15 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Fix version
+//      Build number
+//
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.*")]
