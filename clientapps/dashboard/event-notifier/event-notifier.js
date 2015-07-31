18a0dc8ec5e8bc200f9667f3157498a827181b3e
keigezellig <mh.joosten666@gmail.com>
2015-07-31 16:47:32 +0200

    Issue #10: All config for the event notifier is stored in a config file, also wrapped the whole socket.io thignie in a http server to prevent CORS errors in the browser

--- clientapps/dashboard/event-notifier/event-notifier.js
+++ clientapps/dashboard/event-notifier/event-notifier.js
@@ -1,44 +1,84 @@
 /*
  * Configuration and helpers
  */
-var namespace = '/sensoralerts';
+
+var http = require('http').Server();
+var io = require('socket.io')(http);
 var connectionscounter = 0;
-var serverport = 8989;
-var rabbitExchange = 'FruitHAP_PubSubExchange';
-var topic = 'alerts';
+
 
 /*
- * Project dependencies
- */
-var io = require('socket.io')(serverport);
-var rabbitcontext = new require('rabbit.js').createContext('amqp://admin:admin@192.168.1.80');
-
-/*
- * Collects data
+ * Handle Sockets.io connections
  */
 
-// Implement the methods to handle the new events
-function newEventCallback(eventName, msg) {
+// Event handlers
+io.of(config.namespace).on('connection', function (socket) {
+    // Do some logic for every new connection
+    connectionscounter++;
+    console.log("New client connected. Number of connected clients %s", connectionscounter);
+    
+    // On subscribe events join client to room
+    socket.on('subscribe', function (room) {
+        socket.join(room);
+        console.log("Client joined room: " + room);
+    });
+    
+    // On disconnect events
+    socket.on('disconnect', function (socket) {
+        console.log("Client disconnect.");
+        connectionscounter--;
+    });
+    
+});
 
-	// Invokes propagation
-	propagatesEvent(eventName, JSON.stringify(msg));
-}
+
+
+console.log("Load and check config");
+var config = loadConfig(__dirname + "/config.json");
+
+
+http.listen(config.serverPort, function () {
+    console.log("Starting Node.js server with namespace='%s' on port %s", config.namespace, config.serverPort);
+});
+
+connectToExternalSources();
+
+
 
 // Connection to external data sources
 function connectToExternalSources() {
 	
-    var sub = rabbitcontext.socket('SUB', {routing: 'topic', persistent: true});
-    sub.setEncoding('utf8');
-
-    sub.on('data', function(sensorAlert) {
-
-        console.log("received alert..");
-        var alertObject = JSON.parse(sensorAlert);
-		newEventCallback(alertObject.SensorName, alertObject);
+    var rabbitcontext = new require('rabbit.js').createContext(config.fruitHAPSettings.connectionString);
+    rabbitcontext.on('ready', function () {
+        
+        console.log("Connected to rabbitmq server...");
+        var sub = rabbitcontext.socket('SUB', { routing: 'topic', persistent: true });
+        sub.setEncoding('utf8');
+        
+        sub.on('data', function (sensorAlert) {
+            
+            console.log("received alert..");
+            var alertObject = JSON.parse(sensorAlert);
+            newEventCallback(alertObject, config.namespace, config.rooms);
+        });
+        
+        sub.connect(config.fruitHAPSettings.exchange, config.fruitHAPSettings.topic);
     });
+}
 
-    sub.connect(rabbitExchange,topic);
-
+/*
+ * Collects data
+ */
+// Implement the methods to handle the new events
+function newEventCallback(event, namespace, rooms) {
+    
+    sensorName = event.SensorName;
+    room = getRoomForEvent(sensorName, rooms);
+    
+    if (room != null) {
+        // Invokes propagation
+        propagatesEvent(namespace, room, sensorName, JSON.stringify(event));
+    }
 }
 
 /*
@@ -46,41 +86,45 @@ function connectToExternalSources() {
  */
 
 // Propagates event through all the connected clients
-function propagatesEvent(eventName, event) {
-	if (connectionscounter>0) {
-		io.of(namespace).emit(eventName, event);
-		console.log("New event propagated in: Namespace='%s' EventName='%s' Event='%s'", namespace, eventName, event);
-	}
+function propagatesEvent(namespace, room, eventName, event) {
+    if (connectionscounter > 0) {
+        io.of(namespace).to(room).emit(eventName, event);
+        console.log("New event propagated in: Namespace='%s', Room='%s', EventName='%s' Event='%s'", namespace, room, eventName, event);
+    }
+    else
+        console.log("No connections");
 }
 
-/*
- * Handle Sockets.io connections
- */
+function getRoomForEvent(sensorName, rooms)
+{
+    for (var room in rooms) {
+        for (var sensor in rooms[room].sensors) {
+            if (rooms[room].sensors[sensor] == sensorName) {
+                return rooms[room].name;
+            }
+        }
 
-// Event handlers
-io.of(namespace).on('connection', function(socket) {
-	
-	console.log("New client connected.");
-	
-	// Do some logic for every new connection
-	connectionscounter++;
-	
-	// On subscribe events join client to room
-    socket.on('subscribe', function(room) {
-    	socket.join(room);
-    	console.log("Client joined room: " + room);
-    });
-        
-    // On disconnect events
-    socket.on('disconnect', function(socket) {
-    	console.log("Client disconnect.");
-    	connectionscounter--;
-    });
+    }
+
+    return null;
+}
+
+
+
+
+function loadConfig(filename) {
     
-});
+    var nconf = require('nconf');
+    
+    nconf.use('file', { file: filename });
+    nconf.load();
+    
+    console.log("Loaded configuration: " + JSON.stringify(nconf.get()));
+    
+    return nconf.get();
+}
 
 /*
  *  Run
  */
-console.log("Starting Node.js server with namespace='%s'", namespace);
-connectToExternalSources();
+
