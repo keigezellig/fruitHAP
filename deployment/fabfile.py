3eae2a54408ad0fec10b9d4005a6497f66222325
Maarten Joosten <mh.joosten666@gmail.com>
2015-11-09 23:19:33 +0100

    Issue #51 Added initial deployment script

---
+++ deployment/fabfile.py
@@ -0,0 +1,5 @@
+# Fabric script for FruitHAP on a Raspberry Pi
+# Prequisites: - Base Raspbian image as described here (https://bitbucket.org/keigezellig/fruithap/wiki/Deployment%20plan)
+
+def hello():
+    print("Hello world!")
