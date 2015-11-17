23cc089e33e9b59422eb97a50e7e3a092d1819de
Maarten Joosten <mh.joosten666@gmail.com>
2015-11-17 13:59:20 +0100

    Issue #51 Added initial Ansible playbook for installing prequesites

--- deployment/fabfile.py
+++
@@ -1,5 +0,0 @@
-# Fabric script for FruitHAP on a Raspberry Pi
-# Prequisites: - Base Raspbian image as described here (https://bitbucket.org/keigezellig/fruithap/wiki/Deployment%20plan)
-
-def hello():
-    print("Hello world!")
