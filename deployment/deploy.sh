4c1fa78a938ec2c5c32dab9c029b470118a7fc22
Maarten Joosten <mh.joosten666@gmail.com>
2015-11-21 13:56:01 +0100

    Issue #51 Also support ansible host file

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,14 +1,19 @@
 #! /bin/sh
 
-target=$1
+if [ $# -eq 2 ] ; then
+	target=$1
+	extraopts=$2
+fi
+
+if [ $# -eq 1 ] ; then
+	extraopts=$1
+fi
+
 ansible=$(which ansible-playbook)
 playbookname="fruithap_engine_playbook.yml"
 user=pi
 
-if [ -z $target ] ; then
-	echo "Usage: deploy_engine [target]" >&2
-	exit 1
-fi
+
 
 if [ -z $ansible ] ; then
 	echo "ERROR: Ansible should be installed" >&2
@@ -20,5 +25,12 @@ if [ ! -f $playbookname ] ; then
 	exit 1
 fi
 
-$ansible $playbookname -u $user -s -vvvv -e target=$target
+if [ -z $target ] ; then
+	cmdline="$ansible $playbookname -u $user -s $extraopts"
+else
+	cmdline="$ansible $playbookname -u $user -e target=$target -s $extraopts"
+fi
+
+$cmdline
+
 
