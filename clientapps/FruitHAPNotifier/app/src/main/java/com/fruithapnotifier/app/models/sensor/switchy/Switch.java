cdc8357deccc85cb5a5ce1adc199e2f9f79e224a
Maarten Joosten <mh.joosten666@gmail.com>
2016-05-05 14:20:02 +0200

    Extra error check for a null data element for a switch

--- clientapps/FruitHAPNotifier/app/src/main/java/com/fruithapnotifier/app/models/sensor/switchy/Switch.java
+++ clientapps/FruitHAPNotifier/app/src/main/java/com/fruithapnotifier/app/models/sensor/switchy/Switch.java
@@ -104,7 +104,12 @@ public class Switch extends StatefulSensor
         try
         {
             String sensorName = eventData.getString("SensorName");
-            String typeName = eventData.getJSONObject("Data").getString("TypeName");
+            String typeName = "";
+            if (!eventData.isNull("Data"))
+            {
+                typeName = eventData.getJSONObject("Data").getString("TypeName");
+            }
+
 
             if (sensorName.equals(this.name) && typeName.equals("OnOffValue"))
             {
