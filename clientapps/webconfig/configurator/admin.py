4b7d4dfaf54ed15d6d8c790fc67d610cb0988d4e
Maarten Joosten <mh.joosten666@gmail.com>
2016-08-02 11:07:13 +0200

    Added models for multiple site support

--- clientapps/webconfig/configurator/admin.py
+++ clientapps/webconfig/configurator/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from configurator.models import Site
+
 
 # Register your models here.
+
+class SiteAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'hostname', 'configPort', 'mqPort']
+
+
+admin.site.register(Site, SiteAdmin)
--- clientapps/webconfig/configurator/models.py
+++ clientapps/webconfig/configurator/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Site(models.Model):
+    name = models.CharField('Name', max_length=20)
+    description = models.TextField('Description', blank=True)
+    hostname = models.CharField('Hostname', max_length=20)
+    configPort = models.IntegerField('Configuration port')
+    mqPort = models.IntegerField('Message queue port', blank=True)
