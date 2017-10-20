041d4d8df1eeabe41eeadbab7a85dd63f330ad84
keigezellig <mh.joosten666@gmail.com>
2017-10-20 21:29:48 +0200

    Fixed small error

--- Sensors/FruitHAP.Sensor.FruitSensor/RfxFruitProtocol.cs
+++ Sensors/FruitHAP.Sensor.FruitSensor/RfxFruitProtocol.cs
@@ -6,14 +6,22 @@ namespace FruitHAP.Sensor.FruitSensor
     {
         public RfxFruitValue Decode(uint valueToBeDecoded)
         {
-            RfxFruitValue result;
+            RfxFruitValue result = new RfxFruitValue();
             uint isSignBitSet = valueToBeDecoded & 0x80000;
             uint type = valueToBeDecoded >> 20;
             uint value = valueToBeDecoded & 0x7FFFF;
 
             long endvalue = isSignBitSet != 0 ? -value : value;
 
-            result.Quantity = (RfxFruitQuantity)type;
+            if (Enum.IsDefined(typeof(RfxFruitQuantity), (Int32)type))
+            {
+                result.Quantity = (RfxFruitQuantity)type;    
+            }
+            else
+            {
+                result.Quantity = RfxFruitQuantity.Unknown;
+            }
+            
             result.Value = endvalue;
 
             return result;
@@ -25,14 +33,14 @@ namespace FruitHAP.Sensor.FruitSensor
     {
         public long Value {get; set;}
         public RfxFruitQuantity Quantity { get; set; }
-    };
+    }
 
 
     public enum RfxFruitQuantity
     {
-        Unknown = 0x00,
-        TemperatureInCentiCelsius = 0x01,
-        HumidityInPercentage = 0x02
+        Unknown = 0xFF,
+        TemperatureInCentiCelsius = 0x00,
+        HumidityInPercentage = 0x01
     }
 }
 
